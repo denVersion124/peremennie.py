4b251635831c471c91acfb9b75ded542389df42a
denVersion124 <den.kaleda@bk.ru>
2024-10-11 10:09:43 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Vipolnel_DZ = 12
+Zatrtil_Vremia = 1.5
+Kurs_name = "Python"
+Time_na_1zadanie = Zatrtil_Vremia / Vipolnel_DZ
+print(f"Курс:{Kurs_name}, всего задач: {Vipolnel_DZ}, затрачено часов: {Zatrtil_Vremia}, среднее время выполнения {Time_na_1zadanie} часа.")
